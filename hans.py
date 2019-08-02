ad83687cb4c3ecb4aa629b66a0c4e22a1e3deda2
Matthias <me@sigpwr.de>
2019-08-02 18:24:10 +0200

    Introduce Pipfile, update Hans to work with discord.py version 1.2.3

--- hans.py
+++ hans.py
@@ -1,8 +1,12 @@
-import discord
-import secrets
 import time
 
+import discord
+
+import secrets
+
+
 client = discord.Client()
+
 
 @client.event
 async def on_message(message):
@@ -10,57 +14,63 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(('hans', 'Hans', 'HANS')):
-        msg = '*reicht {0.author.mention} den Flammenwerfer*'.format(message)
-        await client.send_message(message.channel, msg)
+    if message.content.startswith(("hans", "Hans", "HANS")):
+        msg = "*reicht {0.author.mention} den Flammenwerfer*".format(message)
+        await message.channel.send(msg)
     
     if any(client.user.name in s.name for s in message.mentions):
-        msg = 'Sie haben gerufen?'
-        await client.send_message(message.channel, msg)
+        msg = "Sie haben gerufen?"
+        await message.channel.send(msg)
+
+
     # Does the Treppenwitz
-    if message.content.startswith(('!treppe', '!Treppe')):
-        theserver = client.servers
+    if message.content.startswith(("!treppe", "!Treppe")):
+        theserver = client.guilds[0]
         mess = message.content.split()
         timeout = 5
         try:
-            timeout = int(mess[2])
+            timeout = int(mess[-1])
         except (ValueError):
             pass
 
         try:
-            member_to_move = mess[1]
-            member_obj = sorted(theserver)[0].get_member_named(member_to_move)
-            member_voice_chan = member_obj.voice.voice_channel
+            member_to_move = " ".join(mess[1:-1]) # remove first and last param
+            all_members = client.users
+            member_obj = theserver.get_member_named(member_to_move)
+            
+            member_voice_chan = member_obj.voice.channel
 
-            channels = client.get_all_channels()
+            channels = theserver.voice_channels
             silence = None
             for channel in channels:
-                if channel.name.startswith('Stille'):
+                if channel.name.startswith("Stille"):
                     silence = channel
                 else:
                     pass
 
-            msg = 'Ab auf die Treppe mir dir, {}!'.format(member_to_move)
-            await client.send_message(message.channel, msg)
-            await client.move_member(member_obj, silence)
+            msg = "Ab auf die Treppe mir dir, {}!".format(member_to_move)
+            await message.channel.send(msg)
+            await member_obj.move_to(silence, reason="Auszeit!")
 
             if timeout > 10:
                 timeout = 10
             
             time.sleep(timeout)
-            await client.move_member(member_obj, member_voice_chan)
+            await member_obj.move_to(member_voice_chan)
         except (IndexError):
-            msg = 'Somethings wrong with your arguments. Try again! Syntax: !treppe <user> <sekunden>'
-            await client.send_message(message.channel, msg)
+            msg = "Somethings wrong with your arguments. Try again! Syntax: !treppe <user> <seconds>"
+            await message.channel.send(msg)
         except (AttributeError):
-            msg = "Seems I didn't find the user =/ Try again! Syntax: !treppe <user> <sekunden>"
-            await client.send_message(message.channel, msg)
+            msg = "Seems I didn't find the user. Try again! Syntax: !treppe <user> <seconds>"
+            await message.channel.send(msg)
+
 
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    print('------')
+    print("------")
+
 
 client.run(secrets.TOKEN)
